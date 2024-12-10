226ff0d04c3c8dfc12d9f70aa79894c4c8b21f82
brando1607 <109045330+brando1607@users.noreply.github.com>
2024-12-10 14:25:09 -0300

    modify mysql config so it matches env variable

--- user/config/mysql.config.js
+++ user/config/mysql.config.js
@@ -1,6 +1,6 @@
 process.loadEnvFile();
 export const config = {
-  host: process.env.DB_LOCALHOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
