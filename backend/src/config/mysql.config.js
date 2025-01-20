ec63c17662d330f01202d99b5c2ac2f50a1dc7a8
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-01-20 15:16:24 -0300

    db host changed

--- backend/src/config/mysql.config.js
+++ backend/src/config/mysql.config.js
@@ -1,6 +1,6 @@
 process.loadEnvFile();
 export const config = {
-  host: process.env.DB_HOST,
+  host: process.env.MYSQL_DATABASE,
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
