8f673cf8c81a8272cae932733bb0144c7312174c
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-01-20 23:28:52 -0300

    password added for redis

--- backend/src/utils/redisConfig.js
+++ backend/src/utils/redisConfig.js
@@ -7,6 +7,7 @@ export const client = createClient({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
   },
+  password: process.env.REDIS_PASSWORD,
 });
 
 export const redisConnection = async () => {
