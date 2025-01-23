fc30d88a3e9d58daa92530dec0a554e5c1c7af8a
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-01-22 23:08:18 -0300

    swagger config modified

--- backend/src/utils/swagger.js
+++ backend/src/utils/swagger.js
@@ -5,7 +5,8 @@ export const opts = {
     openapi: "3.1.1",
     info: {
       title: "Habit Builder",
-      description: "Documentation for the Habit Builder app.",
+      description:
+        "Documentation for the Habit Builder app. Link to github repository https://github.com/brando1607/habitBuilder",
     },
   },
   apis: [`${__dirname}/docs/*.yaml`],
